e058aab2bd3655c78cdce8ab419ff6b363937e39
ziggyzulu <k00083370@student.lit.ie>
2019-03-02 19:48:04 +0000

    Loads of errors, not worth pursuing

--- firebaseExample/src/pages/home/home.ts
+++ firebaseExample/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {AngularFireDatabase} from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
