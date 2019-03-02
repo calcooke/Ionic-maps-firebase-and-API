f28074cb148cbbb8b5671bc27dd8b9b80a293665
ziggyzulu <k00083370@student.lit.ie>
2019-03-02 21:11:43 +0000

    Can console.log data from firebase

--- firebaseTest3/src/app/app.module.ts
+++ firebaseTest3/src/app/app.module.ts
@@ -6,6 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+var config = {
+  apiKey: "AIzaSyDkwUm605E5id2YTcE_J_nqwhyVQ8jW9WM",
+  authDomain: "testmap-bbc90.firebaseapp.com",
+  databaseURL: "https://testmap-bbc90.firebaseio.com",
+  projectId: "testmap-bbc90",
+  storageBucket: "testmap-bbc90.appspot.com",
+  messagingSenderId: "649800103612"
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +25,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(config),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
