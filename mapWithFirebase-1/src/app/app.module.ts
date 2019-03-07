066134355316584122ba4d38415ab889703a2fb7
ziggyzulu <k00083370@student.lit.ie>
2019-03-07 15:05:23 +0000

    Basic map loading. No markers.

---
+++ mapWithFirebase-1/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import {GoogleMapComponent} from '../components/google-map/google-map';
+
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import {HttpClientModule} from '@angular/common/http';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+//import {Observable} from 'rxjs';
+//import 'rxjs-compat';
+
+
+var config = {
+  apiKey: "AIzaSyDkwUm605E5id2YTcE_J_nqwhyVQ8jW9WM",
+  authDomain: "testmap-bbc90.firebaseapp.com",
+  databaseURL: "https://testmap-bbc90.firebaseio.com",
+  projectId: "testmap-bbc90",
+  storageBucket: "testmap-bbc90.appspot.com",
+  messagingSenderId: "649800103612"
+};
+//firebase.initializeApp(config);
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    GoogleMapComponent
+    
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(config),
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ mapWithFirebase-1/src/components/google-map/google-map.ts
@@ -0,0 +1,49 @@
+import { Component, ViewChild } from '@angular/core';
+
+
+declare var google: any;
+
+@Component({
+  selector: 'google-map',
+  templateUrl: 'google-map.html'
+})
+export class GoogleMapComponent {
+
+  public monuments:any[];
+
+  @ViewChild("map") mapElement;
+  map: any;
+  testMarker: any;
+  google:any;
+  
+  
+  constructor() {
+    console.log('Generating map');
+  }
+
+  ngOnInit(){
+    console.log('Initializing map');
+    this.initMap();
+    
+  }
+
+  initMap(){
+
+    console.log('Setting coords');
+    let coords = new google.maps.LatLng(53.13639186, -9.280849169);
+    
+    console.log('Setting map options');
+    let mapOptions: google.maps.MapOptions = {
+
+      center: coords,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+
+    }
+
+    console.log('Assigning map options to map');  
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    console.log('Map set up and assigned');  
+  }
+
+}
