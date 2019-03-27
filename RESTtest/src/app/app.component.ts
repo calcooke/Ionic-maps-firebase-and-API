4ffef03aedffccb97bd4426c265692db0d534e66
ziggyzulu <k00083370@student.lit.ie>
2019-03-27 11:55:53 +0000

    Retrieving REST data! Too much information

--- RESTtest/src/app/app.component.ts
+++ RESTtest/src/app/app.component.ts
@@ -2,21 +2,30 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+//import {HttpClient} from '@angular/common/http';
+
 
 import { HomePage } from '../pages/home/home';
+import { HttpClient } from '@angular/common/http';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private http: HttpClient) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  ngOnInit(){
+    console.log('Does this onini get called?');
+    let obs = this.http.get('http://webservices.npws.ie/arcgis/rest/services/NM/NationalMonuments/MapServer/0/query?where=entity_id%3D%27cl00567%27&outFields=webnotes&f=pjson')
+    obs.subscribe((response) => console.log(response));
   }
 }
 
